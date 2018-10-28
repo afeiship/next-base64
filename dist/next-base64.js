9cf1c63c0b4e829cfb71701c6cb70a26e45e64d9
afeiship <1290657123@qq.com>
2018-10-28 22:52:14 +0800

    issue: add base64-js for polyfill

--- dist/next-base64.js
+++ dist/next-base64.js
@@ -1,22 +1,38 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var base64 = global.base64 || require('base-64');
+  var supportEncode = 'btoa' in window;
+  var supportDecode = 'atob' in window;
 
   var NxBase64 = nx.declare('nx.Base64', {
     statics: {
-      encode: function (inValue) {
-        return global.btoa(inValue);
+      init: function() {
+        this.encode();
+        this.decode();
       },
-      decode: function (inValue) {
-        return global.atob(inValue);
+      encode: function() {
+        this.encode = supportEncode
+          ? function(inValue) {
+              return global.btoa(inValue);
+            }
+          : function(inValue) {
+              return base64.encode(inValue);
+            };
+      },
+      decode: function() {
+        this.decode = supportDecode
+          ? function(inValue) {
+              return global.atob(inValue);
+            }
+          : function(inValue) {
+              return base64.decode(inValue);
+            };
       }
     }
   });
 
-
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxBase64;
   }
-
-}());
+})();
